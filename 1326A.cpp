e895bbfab1224980691af0b1b94a2066fac9963b
Boranroni <84103778+boranroni@users.noreply.github.com>
2022-07-05 16:56:31 +0300

    Create 1326A.cpp
    very clever solution

---
+++ 1326A.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+   int t, n;
+   cin>>t;
+   
+   while(t--)
+   {
+    cin>>n;
+    if(n == 1) cout<<-1<<endl;
+
+    else
+    {
+        cout<<2;
+        
+        for(int i = 0; i<n-1 ; i++)
+        {
+            cout<<3;
+        }
+        cout<<endl;
+    }
+   }
+    return 0;
+}
