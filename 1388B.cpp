ea8c1bcfd2671d5812322ddc3a0bbecd7384f348
Boranroni <84103778+boranroni@users.noreply.github.com>
2022-07-11 14:45:40 +0300

    Create 1388B.cpp
    should learn binary numbers

---
+++ 1388B.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int t; cin>>t;
+    
+    while(t--)
+    {
+        int n; cin>>n;
+        int m = (n+3)/4;
+        
+        for(int i = 0; i<n-m;i++)
+            cout<<9;
+        
+        for(int i = 0;i<x;i++)
+            cout<<8;
+    
+        cout<<endl;
+    }
+    
+    
+    return 0;
+}
